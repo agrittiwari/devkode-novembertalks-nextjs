52fd698ee4853e11e3d77d31542e0170448a00a9
Agrit Tiwari <agrit1408@gmail.com>
2021-11-16 14:29:18 +0530

    learing nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import 'tailwindcss/tailwind.css'
-import { TodosProvider } from '../contexts/TodosContext'
-import {UserProvider} from '@auth0/nextjs-auth0'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const { user } = pageProps;
-  return (
-    <UserProvider user={user}>
-    <TodosProvider>
-    <div className="container mx-auto my-10 max-w-xl">
-            <Component {...pageProps} />
-         </div>
-  </TodosProvider>
-    
-    </UserProvider>
-    
-  )
-  
-  
+  return <Component {...pageProps} />
 }
 
 export default MyApp
