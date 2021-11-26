141834e3c4c78d7c388a64368a45856102ad093e
Agrit Tiwari <agrit1408@gmail.com>
2021-11-26 23:16:31 +0530

    preparing the whole

--- pages/api/utils/Auth0.js
+++
@@ -1,9 +0,0 @@
-import { initAuth0 } from '@auth0/nextjs-auth0';
-
-export default initAuth0({
-  secret: process.env.AUTH0_SECRET,
-  issuerBaseURL: 'https://your-tenant.auth0.com',
-  baseURL: 'http://localhost:3000',
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET
-});
